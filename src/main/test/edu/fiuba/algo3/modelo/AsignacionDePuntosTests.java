fc6c54ef01a7d43868cb8a6234a8f9f2f925cc8a
AgustinTardaFIUBA <atarda@fi.uba.ar>
2020-07-22 19:42:47 -0300

    Creamos el test para asignarle puntos a los jugadores luego de responder una pregunta de Verdadero y Falso Clasico

--- src/main/test/edu/fiuba/algo3/modelo/AsignacionDePuntosTests.java
+++ src/main/test/edu/fiuba/algo3/modelo/AsignacionDePuntosTests.java
@@ -1,10 +1,26 @@
 package edu.fiuba.algo3.modelo;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class AsignacionDePuntosTests {
     @Test
-    public void testVerdaderoFalsoClasicoRecibeRespuestasYAsignaPuntajes(){
+    public void testVerdaderoFalsoClasicoJugador1AsertaYJugador2FallaAsignandoCorrectamenteLosPuntajes(){
+        String consigna = "El Sol es azul";
+        VerdaderoFalsoClasico preguntaVerderoFalsoClasico = VerdaderoFalsoClasico.crearVerdaderoFalsoFalsa(consigna);
 
+        Kahoot kahoot = new Kahoot();
+        kahoot.setNombreJugador1("Juan");
+        kahoot.setNombreJugador2("Maria");
+        Respuesta respuestaJugador1 = new Respuesta("Falso");
+        Respuesta respuestaJugador2 = new Respuesta("Verdadero");
+
+        kahoot.setRespuestaJugador1(respuestaJugador1);
+        kahoot.setRespuestaJugador2(respuestaJugador2);
+
+        kahoot.evaluarRespuestas(preguntaVerderoFalsoClasico);
+
+        assertEquals(1,kahoot.getPuntajeJugador1());
+        assertEquals(0,kahoot.getPuntajeJugador2());
     }
 }
