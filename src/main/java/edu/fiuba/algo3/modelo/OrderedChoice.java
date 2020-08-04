217ccdb594bab7e3302d6a3634f1960411cf2cd5
Tomas Fernandez Manoukian <tomymanoukian@gmail.com>
2020-08-04 01:08:17 -0300

    feat: agrego clase OrderedChoice y sus test

--- src/main/java/edu/fiuba/algo3/modelo/OrderedChoice.java
+++ src/main/java/edu/fiuba/algo3/modelo/OrderedChoice.java
@@ -1,12 +1,32 @@
 package edu.fiuba.algo3.modelo;
 
 public class OrderedChoice extends Pregunta {
-    public OrderedChoice(String unaConsigna, ListaOpciones unasOpciones) {
+
+    private final ListaOpciones listaDeOpciones;
+
+    public OrderedChoice(String unaConsigna, ListaOpciones unaListaDeOpciones) {
         super();
+        if (unaListaDeOpciones.cantidadDeRespuestas() < 2 || unaListaDeOpciones.cantidadDeRespuestas() > 5) {
+            throw new CantidadDeOpcionesInvalidaException();
+        }
+        consigna = unaConsigna;
+        listaDeOpciones = unaListaDeOpciones;
+    }
+
+    public ListaOpciones getOpciones() {return listaDeOpciones;}
+    public String getConsigna() {
+        return super.getConsigna();
     }
 
     @Override
     public void evaluarRespuestaPara(Respuesta unaRespuesta, Jugador unJugador) {
+    }
 
+
+    public int calcularPuntajePara(ListaOpciones opcionesElegidas) {
+        if(listaDeOpciones.esIgual(opcionesElegidas)){
+            return 1;
+        }
+        return 0;
     }
 }
