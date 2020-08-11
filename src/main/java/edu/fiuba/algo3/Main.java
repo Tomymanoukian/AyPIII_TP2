1b26168d1a56bdbc8583dda0523c3b3a0ed7af0d
Ezequiel <ez.equidave@gmail.com>
2020-08-10 21:24:12 -0300

    Cambio la escena a mostrar

--- src/main/java/edu/fiuba/algo3/Main.java
+++ src/main/java/edu/fiuba/algo3/Main.java
@@ -1,7 +1,9 @@
 package edu.fiuba.algo3;
 
-import edu.fiuba.algo3.vista.InterfazDeUsuario;
+import edu.fiuba.algo3.vista.LayoutMenuBienvenida;
+import edu.fiuba.algo3.vista.LayoutVerdaderoFalso;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -9,9 +11,15 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
+        LayoutVerdaderoFalso vistaVerdaderoFalso = new LayoutVerdaderoFalso();
+        LayoutMenuBienvenida vistaBienvenida = new LayoutMenuBienvenida();
+
         stage.setTitle("Kahoot!");
 
+        Scene scene = new Scene(vistaVerdaderoFalso.getLayout(), 400, 275);
 
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
