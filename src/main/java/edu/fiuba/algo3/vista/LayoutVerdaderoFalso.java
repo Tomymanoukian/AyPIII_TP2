d1d6dfdef55b310498dba28e1c604cf05cb487ba
Ezequiel <ez.equidave@gmail.com>
2020-08-10 21:03:22 -0300

    Creo el archivo.

---
+++ src/main/java/edu/fiuba/algo3/vista/LayoutVerdaderoFalso.java
@@ -0,0 +1,16 @@
+package edu.fiuba.algo3.vista;
+
+import edu.fiuba.algo3.controlador.BotonEmpezarJuegoEventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class LayoutVerdaderoFalso{
+}
