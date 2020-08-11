ff6a1c39274ebf424004b493d5859e4265b382ff
Ezequiel <ez.equidave@gmail.com>
2020-08-10 22:08:48 -0300

    Creo LayoutOrderedChoice

---
+++ src/main/java/edu/fiuba/algo3/vista/LayoutOrderedChoice.java
@@ -0,0 +1,90 @@
+package edu.fiuba.algo3.vista;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import java.util.ArrayList;
+
+public class LayoutOrderedChoice extends VBox {
+
+    private VBox layout;
+
+    public LayoutOrderedChoice() {
+        //Comienza primer renglón
+        Label nombreJugador = new Label("Jugador1");
+        Label tiempo = new Label("00:00");
+        Label bonusX2 = new Label("X2");
+        Label bonusX3 = new Label("X3");
+        Label exclusividad = new Label("Exclusividad");
+
+        VBox contenedorNombreJugador = new VBox(nombreJugador);
+
+        VBox contenedorTiempo = new VBox(tiempo);
+        contenedorTiempo.setStyle("-fx-font-weight: bold");
+
+        HBox contenedorBonus = new HBox(bonusX2, bonusX3, exclusividad);
+        contenedorBonus.setSpacing(3);
+
+        HBox contenedorPrimerRenglon = new HBox(contenedorNombreJugador, contenedorTiempo, contenedorBonus);
+        contenedorPrimerRenglon.setAlignment(Pos.CENTER);
+        contenedorPrimerRenglon.setStyle("-fx-font-size: 1.25em;");
+        contenedorPrimerRenglon.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+        contenedorPrimerRenglon.setSpacing(40);
+        contenedorPrimerRenglon.setPadding(new Insets(10));
+
+        //Comienza cuadro de consigna
+
+        Label consigna = new Label("Ordene los siguientes números:");
+        consigna.setStyle("-fx-font-weight: bold");
+
+        Rectangle rectanguloConsigna = new Rectangle(20, 20, 300, 100);
+        rectanguloConsigna.setFill(Color.LAVENDER);
+
+        StackPane contenedorConsigna = new StackPane(rectanguloConsigna, consigna);
+        contenedorConsigna.setPadding(new Insets(15));
+        contenedorConsigna.setStyle("-fx-font-size: 1.3em;");
+
+        VBox contenedorOpciones = new VBox();
+
+        ArrayList<HBox> listaOpciones = new ArrayList<>();
+
+        /* Así debería ser.
+        for (int i=0; i < 5; i++) {
+            listaOpciones.add(new HBox(unOrderedChoice.get(i)));
+        }
+*/
+
+/*
+        ToggleGroup grupoDeOpciones = new ToggleGroup();
+
+        RadioButton verdadero = new RadioButton("Verdadero");
+        HBox contenedroVerdadero = new HBox(verdadero);
+        contenedroVerdadero.setPadding(new Insets(10));
+        contenedroVerdadero.setBackground(new Background(new BackgroundFill(Color.CORNFLOWERBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+
+        RadioButton falso = new RadioButton("Falso");
+        HBox contenedorFalso = new HBox(falso);
+        contenedorFalso.setPadding(new Insets(10));
+        contenedorFalso.setBackground(new Background(new BackgroundFill(Color.CRIMSON, CornerRadii.EMPTY, Insets.EMPTY)));
+
+        verdadero.setToggleGroup(grupoDeOpciones);
+        falso.setToggleGroup(grupoDeOpciones);
+
+        HBox contenedorHorizontal = new HBox(contenedroVerdadero, contenedorFalso);
+        contenedorHorizontal.setAlignment(Pos.CENTER);
+        contenedorHorizontal.setStyle("-fx-font-weight: bold");
+        contenedorHorizontal.setStyle("-fx-font-size: 1.5em;");
+
+        contenedorHorizontal.setSpacing(10);
+        contenedorHorizontal.setPadding(new Insets(15));
+*/
+        layout = new VBox(contenedorPrimerRenglon, contenedorConsigna/*, contenedorHorizontal*/);
+        layout.setBackground(new Background(new BackgroundFill(Color.GHOSTWHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+    }
+
+    public VBox getLayout() {return layout;}
+}
