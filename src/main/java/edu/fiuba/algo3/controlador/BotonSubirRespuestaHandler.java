7bc607d5c4cc75f7367d85fb632e038cb1563cb3
Ezequiel <ez.equidave@gmail.com>
2020-08-12 02:29:30 -0300

    Agrego la funcionalidad de poder actualizar la vista luego de un cambio

--- src/main/java/edu/fiuba/algo3/controlador/BotonSubirRespuestaHandler.java
+++ src/main/java/edu/fiuba/algo3/controlador/BotonSubirRespuestaHandler.java
@@ -1,10 +1,15 @@
 package edu.fiuba.algo3.controlador;
 
+import edu.fiuba.algo3.modelo.Jugador;
 import edu.fiuba.algo3.modelo.ListaOpciones;
 import edu.fiuba.algo3.modelo.Opcion;
+import edu.fiuba.algo3.vista.LayoutOrderedChoice;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
 
@@ -12,22 +17,26 @@ public class BotonSubirRespuestaHandler implements EventHandler<ActionEvent> {
 
     private int posicion;
     private ListaOpciones listaDeOpciones;
+    private LayoutOrderedChoice layout;
+    private Stage stage;
 
-    public BotonSubirRespuestaHandler (int unaPosicion, ListaOpciones unaListaOpciones) {
+    public BotonSubirRespuestaHandler (int unaPosicion, ListaOpciones unaListaOpciones, Stage unStage) {
         posicion = unaPosicion;
+        stage = unStage;
         listaDeOpciones = unaListaOpciones;
     }
 
     @Override
     public void handle(ActionEvent actionEvent) {
 
-        System.out.println(listaDeOpciones.obtener(posicion).getOpcion());
-
         Opcion opcionVaso = new Opcion(listaDeOpciones.obtener(posicion).getOpcion());
-
         listaDeOpciones.establecer(posicion, listaDeOpciones.obtener(posicion-1));
         listaDeOpciones.establecer(posicion-1, opcionVaso);
 
-        System.out.println(listaDeOpciones.obtener(posicion).getOpcion());
+        Scene scene = new Scene(new LayoutOrderedChoice(listaDeOpciones, stage).getLayout(), 390, 375);
+
+        stage.setTitle("Kahoot!");
+        stage.setScene(scene);
+        stage.show();
     }
 }
