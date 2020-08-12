8a43f95b94227591fa208b7da9e556fd8705a1b5
Ezequiel <ez.equidave@gmail.com>
2020-08-12 02:28:46 -0300

    Agrego la ListaOpciones para probar OrderedChoice

--- src/main/java/edu/fiuba/algo3/Main.java
+++ src/main/java/edu/fiuba/algo3/Main.java
@@ -2,6 +2,8 @@ package edu.fiuba.algo3;
 
 import edu.fiuba.algo3.modelo.Jugador;
 import edu.fiuba.algo3.modelo.Kahoot;
+import edu.fiuba.algo3.modelo.ListaOpciones;
+import edu.fiuba.algo3.modelo.Opcion;
 import edu.fiuba.algo3.vista.*;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -18,9 +20,23 @@ public class Main extends Application {
 
         Kahoot kahoot = new Kahoot(jugador1, jugador2);
 
+        ListaOpciones listaOpciones = new ListaOpciones();
+
+        Opcion opcion1 = new Opcion("Numero 1");
+        Opcion opcion2 = new Opcion("Numero 2");
+        Opcion opcion3 = new Opcion("Numero 3");
+        Opcion opcion4 = new Opcion("Numero 4");
+        Opcion opcion5 = new Opcion("Numero 5");
+
+        listaOpciones.agregar(opcion1);
+        listaOpciones.agregar(opcion2);
+        listaOpciones.agregar(opcion3);
+        listaOpciones.agregar(opcion4);
+        listaOpciones.agregar(opcion5);
+
         LayoutVerdaderoFalso vistaVerdaderoFalso = new LayoutVerdaderoFalso();
         LayoutMenuBienvenida vistaBienvenida = new LayoutMenuBienvenida();
-        LayoutOrderedChoice vistaOrderedChoice = new LayoutOrderedChoice();
+        LayoutOrderedChoice vistaOrderedChoice = new LayoutOrderedChoice(listaOpciones, stage);
         LayoutPuntuaciones vistaPuntuaciones = new LayoutPuntuaciones();
         LayoutMultipleChoice vistaMultipleChoice = new LayoutMultipleChoice();
 
@@ -30,10 +46,9 @@ public class Main extends Application {
 
         vistaOrderedChoice.mostrarVista(layout);
 
-        stage.setTitle("Kahoot!");
-
         Scene scene = new Scene(layout, 390, 375);
 
+        stage.setTitle("Kahoot!");
         stage.setScene(scene);
         stage.show();
     }
