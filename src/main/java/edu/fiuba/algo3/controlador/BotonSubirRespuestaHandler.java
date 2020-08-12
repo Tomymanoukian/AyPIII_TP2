c4e07b0b2d20b7b9ada0bf6d7a80dd5f301c8ae0
Ezequiel <ez.equidave@gmail.com>
2020-08-12 03:40:18 -0300

    Refactorizo el funcionamiento de cómo actualizar la vista

--- src/main/java/edu/fiuba/algo3/controlador/BotonSubirRespuestaHandler.java
+++ src/main/java/edu/fiuba/algo3/controlador/BotonSubirRespuestaHandler.java
@@ -7,23 +7,21 @@ import edu.fiuba.algo3.vista.LayoutOrderedChoice;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
-import java.util.ArrayList;
 
 public class BotonSubirRespuestaHandler implements EventHandler<ActionEvent> {
 
     private int posicion;
     private ListaOpciones listaDeOpciones;
-    private LayoutOrderedChoice layout;
+    private Jugador jugador;
     private Stage stage;
 
-    public BotonSubirRespuestaHandler (int unaPosicion, ListaOpciones unaListaOpciones, Stage unStage) {
+    public BotonSubirRespuestaHandler (int unaPosicion, ListaOpciones unaListaOpciones, Jugador unJugador, Stage unStage) {
         posicion = unaPosicion;
-        stage = unStage;
         listaDeOpciones = unaListaOpciones;
+        jugador = unJugador;
+        stage = unStage;
+
     }
 
     @Override
@@ -33,7 +31,7 @@ public class BotonSubirRespuestaHandler implements EventHandler<ActionEvent> {
         listaDeOpciones.establecer(posicion, listaDeOpciones.obtener(posicion-1));
         listaDeOpciones.establecer(posicion-1, opcionVaso);
 
-        Scene scene = new Scene(new LayoutOrderedChoice(listaDeOpciones, stage).getLayout(), 390, 375);
+        Scene scene = new Scene(new LayoutOrderedChoice(listaDeOpciones, jugador, stage).getLayout(), 390, 375);
 
         stage.setTitle("Kahoot!");
         stage.setScene(scene);
