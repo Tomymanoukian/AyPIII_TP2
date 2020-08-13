1d4011df5acc674da298fa607293075aae0c761c
Ezequiel <ez.equidave@gmail.com>
2020-08-13 03:54:07 -0300

    Quito espacios innecesarios

--- src/main/java/edu/fiuba/algo3/vista/EscenaGroupChoice.java
+++ src/main/java/edu/fiuba/algo3/vista/EscenaGroupChoice.java
@@ -1,23 +1,40 @@
 package edu.fiuba.algo3.vista;
 
-import edu.fiuba.algo3.modelo.Jugador;
-import edu.fiuba.algo3.modelo.Kahoot;
-import edu.fiuba.algo3.modelo.Pregunta;
+import edu.fiuba.algo3.modelo.*;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class EscenaGroupChoice implements Escena {
     private Pane layout;
+    private Pregunta pregunta;
+    private ListaOpciones opcionesGrupoAMostradas;
+    private ListaOpciones opcionesCentroMostradas;
+    private ListaOpciones opcionesGrupoBMostradas;
 
-    public EscenaGroupChoice(Pregunta pregunta, Jugador jugador, Kahoot kahoot, Stage stage) {
 
-        //LayoutEscenaGroupChoice layoutEscenaGroupChoice = new LayoutEscenaGroupChoice(pregunta, jugador, kahoot, stage);
-        //layout = layoutEscenaGroupChoice.getLayout();
+    public EscenaGroupChoice(Pregunta unaPregunta, Jugador jugador, ManejadorDeTurnos manejadorDeTurnos) {
+
+        GroupChoice groupChoice = (GroupChoice) unaPregunta;
+        pregunta = unaPregunta;
+        opcionesCentroMostradas = new ListaOpciones(groupChoice.getOpcionesGrupoA());
+        opcionesCentroMostradas.agregarTodo(groupChoice.getOpcionesGrupoB());
+        opcionesCentroMostradas.desordenar();
+
+        LayoutGroupChoice layoutGroupChoice = new LayoutGroupChoice(unaPregunta, this, jugador, manejadorDeTurnos);
+        layout = layoutGroupChoice.getLayout();
     }
 
     @Override
     public Scene getEscena() {
         return (new Scene(layout));
+    }
+
+//    public ListaOpciones getOpcionesMostradas() {return opcionesMostradas;}
+
+    public void actualizar(Jugador jugador, ManejadorDeTurnos manejadorDeTurnos) {
+
+        Pane layout = (new LayoutGroupChoice(pregunta, this, jugador, manejadorDeTurnos)).getLayout();
+        manejadorDeTurnos.getStage().setScene(new Scene(layout));
     }
 }
