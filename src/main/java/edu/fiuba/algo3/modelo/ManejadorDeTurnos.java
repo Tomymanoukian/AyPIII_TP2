44a64e3532a53dbaeeae4efca9bbca36ce1cce30
Tomas Fernandez Manoukian <tomymanoukian@gmail.com>
2020-08-12 22:29:09 -0300

    Agrego mostrar pantalla final al manejadorDeTurnos

--- src/main/java/edu/fiuba/algo3/modelo/ManejadorDeTurnos.java
+++ src/main/java/edu/fiuba/algo3/modelo/ManejadorDeTurnos.java
@@ -1,6 +1,8 @@
 package edu.fiuba.algo3.modelo;
 
+import edu.fiuba.algo3.vista.LayoutPuntuaciones;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.util.Stack;
 
@@ -11,29 +13,31 @@ public class ManejadorDeTurnos {
     Boolean jugador2Respodio;
     Pregunta pregunta;
     Kahoot kahoot;
+    Stage stage;
 
-    public ManejadorDeTurnos(Stack<Pregunta> pilaDePreguntas, Jugador jugador1, Jugador jugador2, Kahoot kahoot){
+    public ManejadorDeTurnos(Stack<Pregunta> pilaDePreguntas, Jugador jugador1, Jugador jugador2, Kahoot kahoot, Stage stage){
         this.pilaDePreguntas = pilaDePreguntas;
         this.jugador1 = jugador1;
         this.jugador2 = jugador2;
         this.kahoot = kahoot;
         jugador2Respodio = true;
+        this.stage = stage;
     }
 
     public Scene mostrarSiguientePregunta(){
 
         if(pilaDePreguntas.isEmpty() && jugador2Respodio){
-            //return vista de puntos
+            return new Scene(new LayoutPuntuaciones().getLayout());
         }
 
         if(jugador2Respodio) {
             jugador2Respodio = false;
             pregunta = pilaDePreguntas.pop();
-            return CreadorDeVistas.crearSiguienteEscena(pregunta, jugador1, kahoot);
+            return CreadorDeVistas.crearSiguienteEscena(pregunta, jugador1, kahoot, stage);
         }
         else{
             jugador2Respodio = true;
-            return CreadorDeVistas.crearSiguienteEscena(pregunta, jugador2, kahoot);
+            return CreadorDeVistas.crearSiguienteEscena(pregunta, jugador2, kahoot, stage);
         }
     }
 }
