5800c1ba1a4a1811829f6c72869ea31778883b17
Ezequiel <ez.equidave@gmail.com>
2020-08-13 19:25:12 -0300

    Modifico cómo se crean las escenas

--- src/main/java/edu/fiuba/algo3/modelo/CreadorDeVistas.java
+++ src/main/java/edu/fiuba/algo3/modelo/CreadorDeVistas.java
@@ -1,23 +1,44 @@
 package edu.fiuba.algo3.modelo;
 
-import edu.fiuba.algo3.vista.Escena;
-import edu.fiuba.algo3.vista.EscenaVerdaderoFalsoClasico;
+import edu.fiuba.algo3.vista.*;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Stack;
 
 public class CreadorDeVistas {
 
     public static Scene crearSiguienteEscena(Pregunta pregunta, Jugador jugador, ManejadorDeTurnos manejadorDeTurnos) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
 
-        String tipoDePregunta = (pregunta.getClass()).getSimpleName();
+        Escena escena = null;
 
-        Class<?> claseDeLaEscena = Class.forName("edu.fiuba.algo3.vista.Escena" + tipoDePregunta);
-        Constructor<Escena> constructor = (Constructor<Escena>)claseDeLaEscena.getConstructor(Pregunta.class, Jugador.class, ManejadorDeTurnos.class);
-        Escena escena = constructor.newInstance(pregunta, jugador, manejadorDeTurnos);
+        if(pregunta.getClass().getSimpleName().equals(VerdaderoFalsoClasico.class.getSimpleName())){
+
+            //escena = new EscenaVerdaderoFalso();
+
+        }else if (pregunta.getClass().getSimpleName().equals(VerdaderoFalsoConPenalidad.class.getSimpleName())){
+
+            //escena = new EscenaVerdaderoFalso();
+
+        }else if (pregunta.getClass().getSimpleName().equals(MultipleChoiceClasico.class.getSimpleName())){
+
+            escena = new EscenaMultipleChoice(pregunta, jugador, manejadorDeTurnos);
+
+        }else if (pregunta.getClass().getSimpleName().equals(MultipleChoiceParcial.class.getSimpleName())){
+
+            escena = new EscenaMultipleChoice(pregunta, jugador, manejadorDeTurnos);
+
+        }else if (pregunta.getClass().getSimpleName().equals(MultipleChoiceConPenalidad.class.getSimpleName())){
+
+            escena = new EscenaMultipleChoice(pregunta, jugador, manejadorDeTurnos);
+
+        }else if (pregunta.getClass().getSimpleName().equals(OrderedChoice.class.getSimpleName())){
+
+            escena = new EscenaOrderedChoice(pregunta, jugador, manejadorDeTurnos);
+
+        }else if (pregunta.getClass().getSimpleName().equals(GroupChoice.class.getSimpleName())) {
+
+            escena = new EscenaGroupChoice(pregunta, jugador, manejadorDeTurnos);
+        }
 
         return escena.getEscena();
     }
