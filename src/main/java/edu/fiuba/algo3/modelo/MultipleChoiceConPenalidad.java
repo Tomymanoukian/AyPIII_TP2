69e6efe8ff1d8ebefca1bb1020dea87fc5db9871
Ezequiel <ez.equidave@gmail.com>
2020-08-18 17:33:41 -0300

    Agrego espacios

--- src/main/java/edu/fiuba/algo3/modelo/MultipleChoiceConPenalidad.java
+++ src/main/java/edu/fiuba/algo3/modelo/MultipleChoiceConPenalidad.java
@@ -6,6 +6,7 @@ import edu.fiuba.algo3.modelo.excepciones.CantidadDeOpcionesInvalidaException;
 public class MultipleChoiceConPenalidad extends MultipleChoice{
 
     public MultipleChoiceConPenalidad(String unaConsigna, ListaOpciones unasOpcionesCorrectas, ListaOpciones unasOpcionesIncorrectas) {
+
         super();
         if ((unasOpcionesCorrectas.cantidadDeOpciones() + unasOpcionesIncorrectas.cantidadDeOpciones()) < 2 ||
                 (unasOpcionesCorrectas.cantidadDeOpciones() + unasOpcionesIncorrectas.cantidadDeOpciones()) > 5) {
@@ -15,12 +16,15 @@ public class MultipleChoiceConPenalidad extends MultipleChoice{
         opcionesCorrectas = unasOpcionesCorrectas;
         opcionesIncorrectas = unasOpcionesIncorrectas;
     }
+
     public static MultipleChoiceConPenalidad recuperar(JsonObject jsonPregunta) {
+
         String consigna = jsonPregunta.get("consigna").getAsString();
         ListaOpciones opcionesCorrectas = ListaOpciones.recuperar(jsonPregunta.getAsJsonArray("opcionesCorrectas"));
         ListaOpciones opcionesIncorrectas = ListaOpciones.recuperar(jsonPregunta.getAsJsonArray("opcionesIncorrectas"));
         return new MultipleChoiceConPenalidad(consigna, opcionesCorrectas, opcionesIncorrectas);
     }
+
     @Override
     public Puntaje evaluarRespuestaPara(Respuesta respuesta) {
 
@@ -34,8 +38,10 @@ public class MultipleChoiceConPenalidad extends MultipleChoice{
 
     @Override
     public Boolean aceptaExclusividad() {return false;}
+
     @Override
     public JsonObject guardar() {
+
         JsonObject jsonMultipleChoiceConPenalidad = new JsonObject();
         jsonMultipleChoiceConPenalidad.addProperty("tipoDePregunta",MultipleChoiceConPenalidad.class.getName());
         jsonMultipleChoiceConPenalidad.addProperty("consigna", consigna);
