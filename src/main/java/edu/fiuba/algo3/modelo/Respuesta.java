b14295a7b6b888bf8987d7dc9e629ec649b00c51
Ezequiel <ez.equidave@gmail.com>
2020-08-18 17:05:44 -0300

    Quito el método calcularPuntaje() de Respuesta

--- src/main/java/edu/fiuba/algo3/modelo/Respuesta.java
+++ src/main/java/edu/fiuba/algo3/modelo/Respuesta.java
@@ -1,11 +1,7 @@
 package edu.fiuba.algo3.modelo;
 
 public class Respuesta {
-/*
-    public Puntaje calcularPuntaje() {
-        return new Puntaje(0);
-    }
-*/
+
     public Puntaje getPuntaje() {
         return new Puntaje(0);
     }
