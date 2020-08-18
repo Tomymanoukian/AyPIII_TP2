a937ded1cb3247a1bb7f1c593cd4fdc055b72af9
Ezequiel <ez.equidave@gmail.com>
2020-08-18 17:36:39 -0300

    Agrego espacios

--- src/main/java/edu/fiuba/algo3/modelo/ManejadorDeArchivos.java
+++ src/main/java/edu/fiuba/algo3/modelo/ManejadorDeArchivos.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ManejadorDeArchivos {
+
     static String FILENAME_RELATIVE_PATH = "preguntas.json";
     private List<Pregunta> preguntasDelJuego;
 
@@ -20,6 +21,7 @@ public class ManejadorDeArchivos {
     }
 
     public void escribirPreguntas(List<Pregunta> preguntasAEscribir) {
+
         try {
             Gson gson = new Gson();
 
@@ -31,11 +33,10 @@ public class ManejadorDeArchivos {
         } catch (IOException ex) {
             throw new ErrorDeEscrituraException();
         }
-
-
     }
 
     private JsonArray guardar(List<Pregunta> preguntasAEscribir) {
+
         JsonArray jsonArrayPreguntas = new JsonArray();
         for (Pregunta pregunta : preguntasAEscribir) {
             jsonArrayPreguntas.add(pregunta.guardar());
@@ -72,6 +73,7 @@ public class ManejadorDeArchivos {
     }
 
     public void leerPreguntas()  {
+
         try {
             String texto = Files.readString(Path.of(FILENAME_RELATIVE_PATH));
 
