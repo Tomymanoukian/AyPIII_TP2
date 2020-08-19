a48a7ee7992d71d1c0fb2ee8c72f074d4245ac7e
Ezequiel <ez.equidave@gmail.com>
2020-08-19 07:53:57 -0300

    Creo el archivo

---
+++ src/main/java/edu/fiuba/algo3/vista/Constantes.java
@@ -0,0 +1,13 @@
+package edu.fiuba.algo3.vista;
+
+public abstract class Constantes {
+
+    public static final int ANCHO_CONTENEDOR_CONSIGNA = 500;
+    public static final int ALTO_CONTENEDOR_CONSIGNA = 150;
+
+    public static final String COLOR_BOTONES = "2670A3";
+    public static final String COLOR_CINTA = "5296C2";
+    public static final String COLOR_CONTENEDOR_CONSIGNA = "A9DDFF";
+    public static final String COLOR_FONDO = "C8E9FF";
+    public static final String COLOR_OPCIONES = "FAFAFA";
+}
