168917baa9c87239894af649cf3e46d17f0b7dfe
Ezequiel <ez.equidave@gmail.com>
2020-08-19 20:16:38 -0300

    Embellezco el layout

--- src/main/java/edu/fiuba/algo3/vista/Layouts/LayoutGroupChoice.java
+++ src/main/java/edu/fiuba/algo3/vista/Layouts/LayoutGroupChoice.java
@@ -19,9 +19,11 @@ import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
 
+import static edu.fiuba.algo3.vista.Constantes.*;
+
 public class LayoutGroupChoice {
 
-    private Pane layout;
+    private VBox layout;
 
     public LayoutGroupChoice(Pregunta pregunta, EscenaGroupChoice escenaGroupChoice, Jugador jugador, ManejadorDeTurnos manejadorDeTurnos) {
 
@@ -39,15 +41,13 @@ public class LayoutGroupChoice {
 
         ContenedorConsigna contenedorConsigna = new ContenedorConsigna(pregunta);
 
-        HBox contenedorGrupos = new HBox(new Label("Grupo"), new Label (groupChoice.getNombreGrupoA()), new Label (groupChoice.getNombreGrupoB()));
-        contenedorGrupos.setAlignment(Pos.CENTER);
-
         ContenedorBotonEnviar contenedorBotonEnviar = new ContenedorBotonEnviar(jugador, respuesta, manejadorDeTurnos, contenedorPrimerRenglon.getTiempo());
 
         //Se crea el layout final
 
-        layout = new VBox(contenedorPrimerRenglon.getLayout(), contenedorConsigna.getLayout(), contenedorGrupos, contenedorOpciones, contenedorBotonEnviar.getLayout());
-        layout.setBackground(new Background(new BackgroundFill(Color.GHOSTWHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+        layout = new VBox(contenedorPrimerRenglon.getLayout(), contenedorConsigna.getLayout(), contenedorOpciones, contenedorBotonEnviar.getLayout());
+        layout.setBackground(new Background(new BackgroundFill(Color.web(COLOR_FONDO), CornerRadii.EMPTY, Insets.EMPTY)));
+        layout.setAlignment(Pos.TOP_CENTER);
     }
 
     public LayoutGroupChoice(Pregunta pregunta, EscenaGroupChoice escenaGroupChoice, Jugador jugador, ManejadorDeTurnos manejadorDeTurnos, EtiquetaTiempo unaEtiquetaTiempo) {
@@ -66,15 +66,13 @@ public class LayoutGroupChoice {
 
         ContenedorConsigna contenedorConsigna = new ContenedorConsigna(pregunta);
 
-        HBox contenedorGrupos = new HBox(new Label("Grupo"), new Label (groupChoice.getNombreGrupoA()), new Label (groupChoice.getNombreGrupoB()));
-        contenedorGrupos.setAlignment(Pos.CENTER);
-
         ContenedorBotonEnviar contenedorBotonEnviar = new ContenedorBotonEnviar(jugador, respuesta, manejadorDeTurnos, contenedorPrimerRenglon.getTiempo());
 
         //Se crea el layout final
 
-        layout = new VBox(contenedorPrimerRenglon.getLayout(), contenedorConsigna.getLayout(), contenedorGrupos, contenedorOpciones, contenedorBotonEnviar.getLayout());
+        layout = new VBox(contenedorPrimerRenglon.getLayout(), contenedorConsigna.getLayout(), contenedorOpciones, contenedorBotonEnviar.getLayout());
         layout.setBackground(new Background(new BackgroundFill(Color.GHOSTWHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+        layout.setAlignment(Pos.TOP_CENTER);
     }
 
     public Pane getLayout() {
@@ -83,8 +81,23 @@ public class LayoutGroupChoice {
 
     private VBox obtenerContenedorDeOpciones(GroupChoice groupChoice, ListaOpciones listaOpciones, RespuestaDeGrupos respuesta, ArrayList<HBox> listaHBox) {
 
-        HBox nombresGrupos = new HBox(10, new Label("Opciones:"), new Label(groupChoice.getNombreGrupoA(), new Label(groupChoice.getNombreGrupoB())));
-        nombresGrupos.setAlignment(Pos.CENTER);
+        Label opcion = new Label("Opciones");
+        Label grupo1 = new Label (groupChoice.getNombreGrupoA());
+        Label grupo2 = new Label (groupChoice.getNombreGrupoB());
+
+        opcion.setPrefWidth(ANCHO_OPCION-ANCHO_GRUPO*2);
+        opcion.setAlignment(Pos.CENTER);
+        grupo1.setPrefWidth(ANCHO_GRUPO);
+        grupo1.setAlignment(Pos.CENTER);
+        grupo2.setPrefWidth(ANCHO_GRUPO);
+        grupo2.setAlignment(Pos.CENTER);
+
+        HBox contenedorGrupos = new HBox(opcion, grupo1, grupo2);
+
+        contenedorGrupos.setBackground(new Background(new BackgroundFill(Color.web(COLOR_GRUPOS), CornerRadii.EMPTY, Insets.EMPTY)));
+        contenedorGrupos.setMinSize(ANCHO_OPCION, 30);
+        contenedorGrupos.setMaxSize(ANCHO_OPCION, 30);
+        contenedorGrupos.setAlignment(Pos.CENTER_RIGHT);
 
         for (int i = 0; i < listaOpciones.cantidadDeOpciones(); i++) {
 
@@ -94,17 +107,38 @@ public class LayoutGroupChoice {
             botonGrupoA.setToggleGroup(grupoDeBotones);
             botonGrupoA.setOnAction(new BotonAsignarGrupoHandler(listaOpciones.obtener(i), respuesta.getOpcionesSeleccionadasGrupoB(), respuesta.getOpcionesSeleccionadasGrupoA()));
 
+            HBox contenedorBotonGrupoA = new HBox (botonGrupoA);
+            contenedorBotonGrupoA.setPrefWidth(ANCHO_GRUPO);
+            contenedorBotonGrupoA.setAlignment(Pos.CENTER);
+
             RadioButton botonGrupoB = new RadioButton();
             botonGrupoB.setToggleGroup(grupoDeBotones);
             botonGrupoB.setOnAction(new BotonAsignarGrupoHandler(listaOpciones.obtener(i), respuesta.getOpcionesSeleccionadasGrupoA(), respuesta.getOpcionesSeleccionadasGrupoB()));
 
-            listaHBox.add(new HBox(10, new Label(listaOpciones.obtener(i).getOpcion()), botonGrupoA, botonGrupoB));
-            listaHBox.get(i).setAlignment(Pos.CENTER);
+            HBox contenedorBotonGrupoB = new HBox (botonGrupoB);
+            contenedorBotonGrupoB.setPrefWidth(ANCHO_GRUPO);
+            contenedorBotonGrupoB.setAlignment(Pos.CENTER);
+
+            Label nombreOpcion = new Label(listaOpciones.obtener(i).getOpcion());
+            nombreOpcion.setPrefWidth(ANCHO_OPCION-ANCHO_GRUPO*2-10);
+            nombreOpcion.setAlignment(Pos.CENTER_LEFT);
+
+            HBox linea = new HBox(nombreOpcion, contenedorBotonGrupoA, contenedorBotonGrupoB);
+
+            linea.setBackground(new Background(new BackgroundFill(Color.web(COLOR_OPCIONES), CornerRadii.EMPTY, Insets.EMPTY)));
+            linea.setMinSize(ANCHO_OPCION, 30);
+            linea.setMaxSize(ANCHO_OPCION, 30);
+            linea.setAlignment(Pos.CENTER_RIGHT);
+
+            listaHBox.add(linea);
         }
 
         VBox contenedorOpciones = new VBox(10);
-        contenedorOpciones.setPadding(new Insets(10));
+        contenedorOpciones.getChildren().addAll(contenedorGrupos);
         contenedorOpciones.getChildren().addAll(listaHBox);
+
+        contenedorOpciones.setBackground(new Background(new BackgroundFill(Color.web(COLOR_CONTENEDOR_CONSIGNA), CornerRadii.EMPTY, new Insets(-20))));
+        contenedorOpciones.setMaxWidth(ANCHO_OPCION);
         contenedorOpciones.setAlignment(Pos.CENTER);
 
         return contenedorOpciones;
