de914d0126ef0e215020f58a55dbdfe2b16caf2c
Tomas Fernandez Manoukian <tomymanoukian@gmail.com>
2020-08-20 03:09:23 -0300

    Agrego funcionalidad a Salir

--- src/main/java/edu/fiuba/algo3/vista/BarraDeMenu.java
+++ src/main/java/edu/fiuba/algo3/vista/BarraDeMenu.java
@@ -1,6 +1,7 @@
 package edu.fiuba.algo3.vista;
 
 import edu.fiuba.algo3.controlador.OpcionAcercaDeEventHandler;
+import edu.fiuba.algo3.controlador.OpcionSalirEventHandler;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
@@ -18,11 +19,10 @@ public class BarraDeMenu extends MenuBar {
         Menu menuAyuda = new Menu("Ayuda");
 
         MenuItem opcionSalir = new MenuItem("Salir");
-        MenuItem opcionAbrir = new MenuItem("Abrir");
         MenuItem opcionAcercaDe = new MenuItem("Acerca de Kahoot!");
 
-        //OpcionSalirEventHandler opcionSalirHandler = new OpcionSalirEventHandler();
-        //opcionSalir.setOnAction(opcionSalirHandler);
+        OpcionSalirEventHandler opcionSalirHandler = new OpcionSalirEventHandler();
+        opcionSalir.setOnAction(opcionSalirHandler);
 
         OpcionAcercaDeEventHandler opcionAcercaDeHandler = new OpcionAcercaDeEventHandler();
         opcionAcercaDe.setOnAction(opcionAcercaDeHandler);
@@ -32,7 +32,7 @@ public class BarraDeMenu extends MenuBar {
 
         opcionPantallaCompleta.setDisable(true);
 
-        menuArchivo.getItems().addAll(opcionAbrir, new SeparatorMenuItem(), opcionSalir);
+        menuArchivo.getItems().addAll(opcionSalir);
         menuAyuda.getItems().addAll(opcionAcercaDe);
         menuVer.getItems().addAll(opcionPantallaCompleta);
 
